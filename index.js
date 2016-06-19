6c687bc562eb98f1a3d6c90853d47a3e9014c498
James Halliday <mail@substack.net>
2016-06-18 20:57:47 -0500

    write records into metadb for list()

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ var hdex = require('hyperdrive-index')
 var jpeg = require('jpeg-marker-stream')
 var sub = require('subleveldown')
 var to = require('to2')
+var through = require('through2')
+var readonly = require('read-only-stream')
 
-var DEX = 'd', META = 'm'
+var DEX = 'd', META = 'm', PROPS = 'p!'
 
 module.exports = JDEX
 
 function JDEX (opts) {
   var self = this
   if (!(self instanceof JDEX)) return new JDEX(opts)
-  self.metadb = sub(opts.db, META)
+  self.metadb = sub(opts.db, META, { valueEncoding: 'json' })
   self.archive = opts.archive
 
   var propmap = {}
@@ -24,23 +26,37 @@ function JDEX (opts) {
   self.dex = hdex({
     archive: self.archive,
     db: sub(opts.db, DEX),
-    map: function (entry, next) {
-      if (!/\.jpe?g/i.test(entry.name)) return next()
+    map: function (entry, cb) {
+      if (!/\.jpe?g$/i.test(entry.name)) return cb()
       var stream = self.archive.createFileReadStream(entry)
       stream.pipe(jpeg()).pipe(to.obj(write, end))
-      stream.once('error', next)
+      stream.once('error', cb)
+      var value = {}
 
       function write (marker, enc, next) {
         var props = propmap[marker.type] || []
         props.forEach(function (p) {
           var n = get(marker, p[1])
-          console.log(p[0], '=>', n)
+          if (n) value[p[0]] = n
         })
         next()
       }
-      function end () {}
+      function end () {
+        self.metadb.put(PROPS + entry.name, value, cb)
+      }
     }
   })
+}
+
+JDEX.prototype.list = function (cb) {
+  var r = this.metadb.createReadStream({ gt: PROPS, lt: PROPS + '~' })
+  var stream = r.pipe(through.obj(function (row, enc, next) {
+    next(null, {
+      name: row.key.split('!').slice(1).join('!'),
+      properties: row.value
+    })
+  }))
+  return readonly(stream)
 }
 
 function get (obj, keys) {
